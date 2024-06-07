86f01942fc88dcbc673315b66159f3e908392247
kelompokptik <cyberlast173@gmail.com>
2024-06-07 16:20:05 +0700

    introduce dan installation with tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-blue-600 min-h-screen">
+      <div>Hello World</div>
+    </div>
+  );
+}
+
+export default App;
